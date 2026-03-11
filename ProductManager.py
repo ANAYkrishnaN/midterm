48d799a1d964dfecfdba590db7000e1c73a085fd
ANAYkrishnaN <115539670+ANAYkrishnaN@users.noreply.github.com>
2026-03-11 14:06:20 -0400

    Added discount feature by Anay Krishna Nambath

--- ProductManager.py
+++ ProductManager.py
@@ -5,3 +5,7 @@ class ProductManager:
 
     def add_product(self, name, price):
         self.products.append({"name": name, "price": price})
+
+    # This method calculates discount price for a product
+    def calculateDiscount(self, price, discount_percent):
+        return price - (price * discount_percent / 100)
